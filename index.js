504a9cbc9029f9b19166919f9b35d14839c405a4
Suprotik007 <csuprotik@gmail.com>
2025-07-21 16:35:40 +0600

    email sent successfull

--- index.js
+++ index.js
@@ -15,16 +15,53 @@ app.get('/',(req,res)=>{
     res.send('kam koratse')
   
 })
+  
+const emailTransporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+  
+    user: process.env.email,
+    pass: process.env.email_pass,
+  },
+});
  
 
 // email route
 app.get('/send-email',async(req,res)=>{
     const paymentInfo={
         transactionId :'xxxyyyzzz',
-        user: 'scpchy89@gmail.com'
+        user: 'scpchy89@gmail.com',
+        parcelInfo :'5 kilograms of hazzlenuts',
+        amount : '2 packs',
+
 
     }
 
+    const emailObject={
+        from: process.env.email,
+        to:paymentInfo.user,
+        subject:'xyzAgroLtd invoice ',
+        html:`
+        <p> Thank you for visiting us. We hope our product becomes the reason of your satisfaction' </p>
+        <br/>
+        <br/>
+        <h3>Transaction Id : ${paymentInfo.transactionId}</h3>
+        <h3>Parcel Info  : ${paymentInfo.parcelInfo}</h3>
+
+        `
+    }
+    
+    try{
+        const emailInfo=await emailTransporter.sendMail(emailObject)
+        console.log('email sent to',emailInfo.messageId);
+        res.send({result:'successful'})
+        
+    }
+    catch(err){
+        console.log(err);
+        res.send('failed email sending')
+        
+    }
 })
 
 app.listen(port,()=>{
@@ -32,11 +69,3 @@ app.listen(port,()=>{
     
 })
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.email,
-    pass: process.env.email_pass,
-  },
-});
